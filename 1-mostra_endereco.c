184d39d9a243c8903f4e8007f01b2ac62b8885da
Matheus Lima <75711232+MathsLima@users.noreply.github.com>
2024-08-28 17:13:29 -0300

    Delete 1-mostra_endereco.c

--- 1-mostra_endereco.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    int b = 200;
-    int c = 1580;
-    printf("%p\n", &b);  //operador & retorna o endereço do seu operando
-    printf("%p\n", &c);    //%p exibe o valor como um end. de memória em hexa
-}
